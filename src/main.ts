32f74f20da0fa18a5ce3136d436997857f1dc47f
gurprit <gurprit.sahota@gmail.com>
2026-09-02 16:36:52 +0100

    Install shared feather coin and shell effects

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,22 @@ import Phaser from 'phaser'
 import { gameConfig } from './game/config/gameConfig'
 import { installBananaTransparency } from './game/items/installBananaTransparency'
 import { installCoinHudAnchor } from './game/items/installCoinHudAnchor'
+import { installFeatherPowerUp } from './game/items/installFeatherPowerUp'
+import { installGreenShellTransparency } from './game/items/installGreenShellTransparency'
 import { installItemCourseEnhancements } from './game/items/installItemCourseEnhancements'
 import { installItemVisualFixes } from './game/items/installItemVisualFixes'
 import { installLightningPowerUp } from './game/items/installLightningPowerUp'
 import { installWorldPickupParticles } from './game/items/installWorldPickupParticles'
+import { installSharedItemEffects } from './game/network/installSharedItemEffects'
 
 installItemVisualFixes()
+installGreenShellTransparency()
 installBananaTransparency()
 installItemCourseEnhancements()
 installWorldPickupParticles()
 installLightningPowerUp()
+installFeatherPowerUp()
+installSharedItemEffects()
 installCoinHudAnchor()
 new Phaser.Game(gameConfig)
 
